fc60691ffc2d1b1f5ef48f5387b02de46b31a723
Gary Garcia <garcia.cruzgc@gmail.com>
2025-09-09 17:14:11 -0400

    Eliminacion de un doctype que no se necesita,  que fue para pruebas

--- mdx_lcdp/mdx_lcdp/doctype/prueba_doctype/prueba_doctype.py
+++
@@ -1,9 +0,0 @@
-# Copyright (c) 2025, Gary Gabriel Garcia Cruz and contributors
-# For license information, please see license.txt
-
-# import frappe
-from frappe.model.document import Document
-
-
-class pruebadoctype(Document):
-	pass
